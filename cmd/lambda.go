f545703eaceb7628479bebaabd63d0a35c116e76
kaumnen <dev@kaumnen.com>
2024-07-07 17:26:25 +0200

    initial

---
+++ cmd/lambda.go
@@ -0,0 +1,21 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// lambdaCmd represents the lambda command
+var lambdaCmd = &cobra.Command{
+	Use:   "lambda",
+	Short: "Use this command to list all lambda functions in your AWS account.",
+	Long:  `This command will list all lambda functions in every region in your AWS account.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("lambda called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(lambdaCmd)
+}
